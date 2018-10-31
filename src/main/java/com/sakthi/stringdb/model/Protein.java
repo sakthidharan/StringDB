aef3f52c63df2257340f9f8e1238f3bbce18f9c1
COMPUTER <COMPUTER@COMPUTER-PC>
2018-10-31 23:53:15 +0530

    commit from netbeans

--- src/main/java/com/sakthi/stringdb/model/Protein.java
+++ src/main/java/com/sakthi/stringdb/model/Protein.java
@@ -2,12 +2,14 @@ package com.sakthi.stringdb.model;
 
 import javax.persistence.Entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
 public class Protein extends RootModel {
 
 	private String name;
