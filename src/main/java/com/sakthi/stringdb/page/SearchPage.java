f458f47f5ceb58c3f24a3a0afc3b408db4820f5c
sakthidharan <sakthidharan@localhost>
2018-11-05 17:19:37 +0530

    Updated search page form entry to imitate exact human actions

--- src/main/java/com/sakthi/stringdb/page/SearchPage.java
+++ src/main/java/com/sakthi/stringdb/page/SearchPage.java
@@ -1,7 +1,9 @@
 package com.sakthi.stringdb.page;
 
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
@@ -22,7 +24,13 @@ public class SearchPage extends StringDbPage {
 		waitUntilFullPageIsLoaded();
 		try {
 			d.findElementByCssSelector("#primary_input\\:single_identifier").sendKeys(proteinName);
-			d.findElementByCssSelector("#species_text_single_identifier").sendKeys(organismName);
+			d.findElementByCssSelector("#organism_text_input_single_identifier > div:nth-child(2) > div:nth-child(2)")
+					.click();// drop down click
+			WebElement organismInputDropDownElement = d.findElementByCssSelector("#speciesList_single_identifier");
+			Select organismSelect = new Select(organismInputDropDownElement);
+			organismSelect.selectByValue("83332");
+			d.findElementByCssSelector("#speciesFloatingDiv_single_identifier > div:nth-child(1) > button:nth-child(5)")
+					.click();// after option selected in drop down, click it's own select button
 			d.findElementByCssSelector("#input_form_single_identifier").submit();
 		} catch (NoSuchElementException e) {
 			log.error(e.getMessage());
